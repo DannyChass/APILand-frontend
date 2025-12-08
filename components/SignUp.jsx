a521e10efa7e8e07776546e2045c5b0dd79e344c
Daniel <daniel.chassagn@gmail.com>
2025-12-08 16:43:25 +0100

    Refactor SignUp form and improve UI components
    Refactored the SignUp component to use React state and handle user registration with validation and error handling. Updated Button and InputText components to support onClick and onChange props for better reusability. Added 'cors' dependency in package.json.

--- components/SignUp.jsx
+++ components/SignUp.jsx
@@ -1,14 +1,24 @@
+<<<<<<< Updated upstream
+=======
+"use client"
+
+>>>>>>> Stashed changes
 import Link from "next/link";
 import Button from "./ui/Button";
 import InputText from "./ui/inputText";
 import Header from "./Header";
+<<<<<<< Updated upstream
 import Backdrop from "@mui/material/Backdrop";
+=======
+import { useState } from "react";
+>>>>>>> Stashed changes
 
 export default function SignUp() {
   return (
     <div className="flex flex-col w-screen h-screen">
       <Header />
 
+<<<<<<< Updated upstream
       <div className="container">
         <div
           style={{ backgroundImage: "url('../background.jpg')", objectFit:'cover' }}
@@ -66,6 +76,57 @@ export default function SignUp() {
               classname=" bg-[#ffffff] rounded-[5]  text-stone-400 h-10 pl-10 shadow w-full hover:bg-[#eeeeee]"
             />
           </div>
+=======
+export default function SignUp() {
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleSignup = async () => {
+    setError(null);
+
+    if (password !== confirmPassword) {
+      setError("Les mots de passes ne correspondent pas");
+      return
+    }
+
+    const response = await fetch("http://localhost:3000/users/signup", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        username,
+        email,
+        password,
+      })
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+
+    if (!data.result) {
+      setError(data.error);
+      return
+    }
+
+    localStorage.setItem("token", data.token);
+
+    alert("Inscription réussi");
+  }
+
+  return (
+    <div className="flex flex-col w-screen h-screen">
+      <header className="pr-10 pl-10 bg-white w-full h-20 items-center flex justify-between">
+        <Link href="#" className="logo"> APIhub</Link>
+        <div className="gap-5 justify-between flex">
+        </div>
+      </header>
+
+      <div className="container">
+>>>>>>> Stashed changes
 
           <div className="w-[70%] justify-items-center items-center">
             <Link
@@ -80,6 +141,86 @@ export default function SignUp() {
           </div>
           <hr className="bg-white border-white border-2 w-[80%]"></hr>
 
+<<<<<<< Updated upstream
+=======
+        <div className="w-[50%] justify-end flex flex-col  h-full p-10">
+
+          <div className=" items-center flex flex-col  h-[40%] ">
+            <h1 className="font-extrabold text-4xl text-stone-500">APIHub,</h1>
+            <h3 className="font-bold text-stone-400 text-2xl">
+              La plateforme des développeurs
+            </h3>
+            <h5 className="text-stone-500 font-medium text-2xl p-10 text-center">
+              Une seule clé, une communauté, des milliers de possibilités.
+            </h5>
+          </div>
+        </div>
+        <div className="flex flex-col bg-[#050F2A] w-[50%] h-full pt-10 items-center gap-[15] text-white">
+          <h3 className="text-2xl w-[60%] font-bold">Sign Up!</h3>
+
+          <p className="text-red-400 font-semibold w-[60%]">
+            {error}
+          </p>
+
+          <div className="flex flex-col w-[60%]">
+            <label htmlFor='Username'>Username</label>
+            <InputText
+              placeHolder="Username"
+              label='email'
+              Name='Username'
+              Type="text"
+              classname=" bg-[#ffffff] rounded-[5]  text-stone-400 h-10 pl-10 shadow w-full hover:bg-[#eeeeee]"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div className="flex flex-col w-[60%]">
+            <label htmlFor='password'>Password</label>
+            <InputText
+              placeHolder="Password"
+              label='email'
+              Name='password'
+              Type="password"
+              classname=" bg-[#ffffff] rounded-[5]   text-stone-400 h-10 pl-10 shadow w-full hover:bg-[#eeeeee]"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="flex flex-col w-[60%]">
+            <label htmlFor='Password'>Confirm Password</label>
+            <InputText
+              placeHolder="Confirm Password"
+              label='email'
+              Name='Password'
+              Type="password"
+              classname=" bg-[#ffffff] rounded-[5]   text-stone-400 h-10 pl-10 shadow w-full hover:bg-[#eeeeee]"
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+          </div>
+          <div className="flex flex-col w-[60%]">
+            <label htmlFor='email'>Email</label>
+            <InputText
+              placeHolder="Email"
+              label='email'
+              Name='email'
+              Type="email"
+              classname=" bg-[#ffffff] rounded-[5]  text-stone-400 h-10 pl-10 shadow w-full hover:bg-[#eeeeee]"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+
+          <div className="w-[70%] justify-items-center items-center">
+            <Link href="#" className="text-sm text-stone-300 hover:underline underline-offset-2">
+              Already sign up ?
+            </Link>
+            <Button
+              classname="w-full bg-[#B8A9FF] h-[50px] font-semibold text-lg rounded-[3] hover:bg-[#9d90de] cursor-pointer"
+              onClick={handleSignup}
+            >
+              Sign Up
+            </Button>
+          </div>
+          <hr className="bg-white border-white border-2 w-[80%]"></hr>
+
+>>>>>>> Stashed changes
           <div className="w-[60%] h-[20%] flex flex-col gap-[20] justify-center items-center">
             <Button classname="bg-white w-full justify-center rounded-[3] items-center gap-3 flex text-stone-400 h-10 hover:bg-stone-200">
               {" "}
@@ -93,5 +234,10 @@ export default function SignUp() {
         </div>
       </div>
     </div>
+<<<<<<< Updated upstream
   );
 }
+=======
+  )
+}
+>>>>>>> Stashed changes
--- components/ui/Button.jsx
+++ components/ui/Button.jsx
@@ -1,10 +1,22 @@
+"use client"
+
 export default function Button(props) {
-    console.log(props.classname!==undefined);
-    if(props.classname!==undefined){
-        return <button className={props.classname}>{props.children}</button>
+    const { classname, children, onClick } = props;
+
+    if (classname !== undefined) {
+        return (
+            <button className={classname} onClick={onClick}>
+                {children}
+            </button>
+        );
     }
+
     return (
-        <button className= "bg-[#B8A9FF] rounded-[3] text-[#F2FDFF] text-sm w-[150px]   p-3 text-shadow-2xs hover:bg-[#9d90de] cursor-pointer ">{props.children}</button>
-        
-    )
+        <button
+            className="bg-[#B8A9FF] rounded-[3] text-[#F2FDFF] text-sm w-[150px] p-3 text-shadow-2xs hover:bg-[#9d90de] cursor-pointer"
+            onClick={onClick}
+        >
+            {children}
+        </button>
+    );
 }
