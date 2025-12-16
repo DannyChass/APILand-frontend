6d7d64b4158fcc40d3811cd1e841f1bceddfa8b0
Matt <matthk17@gmail.com>
2025-12-16 15:40:44 +0100

    feat filter
    Update Dropdown button

--- components/ui/DropDownButton.jsx
+++ components/ui/DropDownButton.jsx
@@ -7,7 +7,7 @@ import * as React from "react";
 import Link from "next/link";
 
 
-function DropDownButton({ title, menu, className }) {
+function DropDownButton({ title, menu, className, onSelect }) {
     const [active, setActive] = useState(false);
 
 
@@ -15,32 +15,39 @@ function DropDownButton({ title, menu, className }) {
         setActive(!active)
     }
 
-    const menuItems = menu.map((item, index) => {
-        const itemUrl = '#'
+    const handleItemClick = (itemValue) => {
+        // 1. Appel de la fonction onSelect passée par le parent (ApiSearch)
+        if (onSelect) {
+            onSelect(itemValue);
+        }
+        // 2. Fermeture du menu déroulant
+        setActive(false);}
+
+        const menuItems = menu.map((item, index) => {
+            
+            return (
+                <li key={index} className="px-4 py-2 hover:bg-gray-100 flex items-center gap-2 active:bg-slate-200 active:shadow " onClick={()=>handleItemClick(item)}>
+                    <span className='flex gap-5 items-center w-full'>
+                        {item}
+                    </span>
+                </li>
+            );
+        })
+
         return (
-            <li key={index} className="px-4 py-2 hover:bg-gray-100 flex items-center gap-2 active:bg-slate-200 active:shadow ">
-                <Link className='flex gap-5 items-center w-full' href={itemUrl} >
-                    {item}
-                </Link>
-            </li>
+            <div className='relative w-full' >
+                <button className={`dropDownButton ${className}`} id={title}
+                    onClick={toggleDropdown}>
+                    {title} <FontAwesomeIcon icon={faAngleDown} />
+                </button>
+                {active && (
+                    <div className="absolute left-0 top-full bg-white shadow-lg rounded-[3] z-50">
+                        <ul className="py-2 text-sm text-gray-700">
+                            {menuItems}
+                        </ul>
+                    </div>)}
+            </div>
         );
-    })
-    console.log(active)
+    }
 
-    return (
-        <div className='relative w-full' >
-            <button className={`dropDownButton ${className}`} id={title}
-                onClick={toggleDropdown}>
-                {title} <FontAwesomeIcon icon={faAngleDown} />
-            </button>
-            {active && (
-                <div className="absolute left-0 top-full bg-white shadow-lg rounded-[3] z-50">
-                    <ul className="py-2 text-sm text-gray-700">
-                        {menuItems}
-                    </ul>
-                </div>)}
-        </div>
-    );
-}
-
-export default DropDownButton;
+    export default DropDownButton;
