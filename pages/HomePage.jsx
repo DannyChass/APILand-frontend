57d7c1693647156f893664c4d1b47c0511c00444
Tourisk69 <geoffrey.demos@gmail.com>
2025-12-19 14:54:40 +0100

    ml search page

--- pages/HomePage.jsx
+++ pages/HomePage.jsx
@@ -261,7 +261,7 @@ function Home() {
         cursor-pointer active:scale-95
       "
             >
-              {allCategory ? 'Hide All Categories' : 'View All Categories'}
+              {allCategory ? 'Hide All Categor' : 'View All Categories'}
             </p>
           </div>
           <div className="w-full flex flex-col gap-10">
